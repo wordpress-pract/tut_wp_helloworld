bbc0027aafe820a590a9e27a1c5da5a828c4c1f3
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-07-11 22:29:09 +0700

    Enqueue Blocks assets and show console.log for testing

---
+++ wordpress/wp-content/plugins/recipe/blocks/README.md
@@ -0,0 +1,2 @@
+#Run dev Blocks
+$ npm run dev
--- wordpress/wp-content/plugins/recipe/index.php
+++ wordpress/wp-content/plugins/recipe/index.php
@@ -27,6 +27,7 @@ include('process/filter-content.php');
 include('includes/front/enqueue.php');
 include('process/rate_recipe.php');
 include('includes/admin/init.php');
+include('blocks/enqueue.php');
 
 // Hooks
 register_activation_hook(__FILE__, 'r_activate_plugin');
@@ -37,5 +38,7 @@ add_action( 'wp_enqueue_scripts', 'r_enqueue_scripts', 100 );
 add_action('wp_ajax_r_rate_recipe', 'r_rate_recipe');
 add_action('wp_ajax_nopriv_r_rate_recipe', 'r_rate_recipe');
 add_action('admin_init', 'recipe_admin_init');
+
+add_action('enqueue_block_editor_assets', 'r_enqueue_block_editor_assets');
 
 // Shortcodes
