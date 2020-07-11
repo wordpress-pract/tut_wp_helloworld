1805486e1dc443bd32ad2d1177d23b46b7a8a25c
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-07-11 14:17:09 +0700

    8. Use Filter Hook to add Rating-HTML to post-content

--- wordpress/wp-content/plugins/recipe/index.php
+++ wordpress/wp-content/plugins/recipe/index.php
@@ -22,10 +22,13 @@ if (!function_exists('add_action')) {
 include('includes/activate.php');
 include('includes/init.php');
 include('process/save-post.php');
+include('process/filter-content.php');
+
 
 // Hooks
 register_activation_hook(__FILE__, 'r_activate_plugin');
 add_action('init', 'recipe_init'); // Help to setup the plugin
 add_action('save_post_recipe', 'r_save_post_admin', 10, 3); // This is new fn from 3.7, called, dynamic hook, trigger on action save_post of a post-recipe-type
+add_filter('the_content', 'r_filter_recipe_content');
 
 // Shortcodes
---
+++ wordpress/wp-content/plugins/recipe/process/recipe-template.php
@@ -0,0 +1,11 @@
+<ul class="list-unstyled">
+    <li>
+        <strong>RATE_I18N: </strong>
+        <div 
+            id="recipe_rating"
+            class="rateit"
+            data-recipe-resetable="false"
+            data-rid="RECIPE_ID"
+        ></div>
+    </li>
+</ul>
