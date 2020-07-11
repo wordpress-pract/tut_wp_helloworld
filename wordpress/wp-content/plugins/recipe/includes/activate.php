3776d4b4ebdfe7174ef2f37df74c9251d0d7d2de
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-07-11 14:39:51 +0700

    10. Create recipe_ratings table through Activate action

--- wordpress/wp-content/plugins/recipe/includes/activate.php
+++ wordpress/wp-content/plugins/recipe/includes/activate.php
@@ -5,4 +5,21 @@ function r_activate_plugin()
     if (version_compare(get_bloginfo('version'), '5.0', '<')) {
         wp_die( __("You must update WordPress to use this plugin", 'recipe') );
     }
+
+    global $wpdb;
+    $prefix = $wpdb->prefix;
+    $charset_collate = $wpdb->get_charset_collate();
+    $createSQL = "
+        CREATE TABLE `" . $prefix . "recipe_ratings` (
+            `ID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+            `recipe_id` BIGINT(20) UNSIGNED NOT NULL,
+            `rating` FLOAT(3,2) UNSIGNED NOT NULL,
+            `user_ip` VARCHAR(50) NOT NULL,
+            PRIMARY KEY (`ID`)
+        )
+        ENGINE=InnoDB " . $charset_collate . " ;
+    ";
+
+    require( ABSPATH . "/wp-admin/includes/upgrade.php" );
+    dbDelta( $createSQL);
 }
